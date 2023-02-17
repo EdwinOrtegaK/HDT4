54781983e096cef747d8536420f5efe2fc037d05
EdwinOrtegaK <andyortegakou@yahoo.com.mx>
2023-02-17 13:22:43 -0600

    Work

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import structure5.SinglyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        Reader leer = new Reader();
+        leer.read();
+
+
+        System.out.println();
+    }
+}
